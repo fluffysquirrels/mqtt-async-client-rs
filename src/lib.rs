29de385e1eaf525802a5c9cc2935e9a017b9951e
Alex Helfet <alex.helfet@gmail.com>
2019-12-29 02:17:55 +0000

    Module comment for src/lib.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! An MQTT 3.1.1 client written in Rust.
+//!
+//! For example usage see the command-line test app at `src/bin/mqttc.rs`
+
 #![deny(warnings)]
 
 // The futures_util::select! macro needs a higher recursion_limit
