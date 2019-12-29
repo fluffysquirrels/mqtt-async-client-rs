6bfba339dc462268f4009fe2418249538da365d6
Alex Helfet <alex.helfet@gmail.com>
2019-12-29 02:17:15 +0000

    First integration test.

---
+++ tests/integration_test.rs
@@ -0,0 +1,52 @@
+#![deny(warnings)]
+
+use mqtt_client::{
+    client::{
+        Client,
+        Publish,
+        QoS,
+        Subscribe,
+        SubscribeReturnCodes,
+        SubscribeTopic,
+    },
+    Result,
+};
+use tokio;
+
+#[test]
+fn pub_and_sub() -> Result<()> {
+    let mut rt = tokio::runtime::Runtime::new()?;
+    rt.block_on(hi2())
+}
+
+async fn pub_and_sub2() -> Result<()> {
+    let mut c = client()?;
+    c.connect().await?;
+
+    // Subscribe to "a"
+    let subopts = Subscribe::new(vec![
+        SubscribeTopic {qos: QoS::AtMostOnce, topic_path: "a".to_owned() }
+    ]);
+    let subres = c.subscribe(subopts).await?;
+    let any_failed = subres.return_codes().iter().any(|rc| *rc == SubscribeReturnCodes::Failure);
+    if any_failed {
+        return Err(format!("Some subscribes failed: {:#?}", subres.return_codes()).into());
+    }
+
+    // Publish to "a"
+    let mut p = Publish::new("a".to_owned(), "x".as_bytes().to_vec());
+    p.set_qos(QoS::AtMostOnce);
+    c.publish(&p).await?;
+
+    // Read from "a".
+    let r = c.read_subscriptions().await?;
+    assert_eq!(r.topic(), "a");
+    assert_eq!(r.payload(), b"x");
+    Ok(())
+}
+
+fn client() -> Result<Client> {
+    Client::builder()
+        .set_host("localhost".to_owned())
+        .build()
+}
